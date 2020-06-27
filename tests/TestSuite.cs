04a8cd319f18882570c5f2b1c22046acfcd219f6
Masakhane Matanzima <sihle.matanzima@gmail.com>
2020-06-27 13:54:44 +0200

    MS test project setup

---
+++ tests/TestSuite.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test_Automation
+{
+    [TestClass]
+    public class TestSuite
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
