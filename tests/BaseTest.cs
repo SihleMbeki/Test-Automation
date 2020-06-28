76922051efc83c023b94bc610344243d5b2fc606
Masakhane Matanzima <sihle.matanzima@gmail.com>
2020-06-28 13:39:09 +0200

    Web driver instance

--- tests/BaseTest.cs
+++ tests/BaseTest.cs
@@ -1,5 +1,6 @@
 using AventStack.ExtentReports;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
 using System;
 using Test_Automation.utilities;
 
@@ -11,6 +12,8 @@ namespace Test_Automation.tests
 {
     public class BaseTest
     {
+        //create webdriver instance
+        protected WebDriverInstance driver;
         //Set universal directory for the current test run
         public static string directory;
         protected ExtentTest testReport { get; set; }
@@ -22,9 +25,18 @@ namespace Test_Automation.tests
         {
             //Extract test name to be used in the report
             testname = TestContext.TestName;
+            //verify that the directory is not created by first test iteration
+            if(directory==null){
             directory = Environment.CurrentDirectory + @"\..\..\..\reports\";
             directory += DateTime.Now.ToString("dd MM yyyy HH:mm:ss").Replace(":", "_").Replace(" ", "_");
             System.IO.Directory.CreateDirectory(directory);
+            }
+
+            //create chrome browser instance
+            driver = new WebDriverInstance();
+            driver.createWebDriver();
+            driver.getDriver().Navigate().GoToUrl("https://www.google.com/");
+            driver.getDriver().Manage().Window.Maximize();
 
             //retrieve new test instance
             testReport = TestReport.createTest(testname);
@@ -45,12 +57,19 @@ namespace Test_Automation.tests
                     break;
                 case UnitTestOutcome.Passed:
                     testReport.Log(Status.Pass, testname + " Passed");
+                     testReport.Pass("Passed",MediaEntityBuilder.CreateScreenCaptureFromPath(driver.screenShot()).Build());
                     break;
                 default:
                     testReport.Log(Status.Error, "Error occured");
                     break;
             }
+
             TestReport.closeReport();
+
+            if (driver.getDriver() != null)
+            {
+                driver.getDriver().Close();
+            }
         }
     }
 }
